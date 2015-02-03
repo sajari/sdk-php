787b033ae9c40bb6ec3640de5fd4242770c8c8ab
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2015-02-03 14:06:31 +1100

    Add fully working example of adding/searching

---
+++ examples/1/add.php
@@ -0,0 +1,25 @@
+<?php
+require 'vendor/autoload.php';
+
+use Sajari\Engine\EngineClient;
+use Sajari\Engine\Exception\EngineException;
+
+$c = new EngineClient(new Guzzle\Http\Client(), array(
+    'access_key' => getenv('SAJARI_ACCESS_KEY'),
+    'secret_key' => getenv('SAJARI_SECRET_KEY'),
+    'company'    => getenv('SAJARI_COMPANY'),
+    'collection' => getenv('SAJARI_COLLECTION'),
+));
+
+try {
+    $r = $c->add(array(
+        'q' => implode(' ', array(getenv('FIRST_NAME'), getenv('LAST_NAME'))),
+        'meta' => array(
+            'first_name' => getenv('FIRST_NAME'),
+            'last_name'  => getenv('LAST_NAME'),
+        ),
+    ));
+    echo var_export($r, true), PHP_EOL;
+} catch (EngineException $e) {
+    echo "There was an error adding the document.", $e->getMessage(), PHP_EOL;
+}
---
+++ examples/1/search.php
@@ -0,0 +1,22 @@
+<?php
+require 'vendor/autoload.php';
+
+use Sajari\Engine\EngineClient;
+use Sajari\Engine\Exception\EngineException;
+
+$c = new EngineClient(new Guzzle\Http\Client(), array(
+    'access_key' => getenv('SAJARI_ACCESS_KEY'),
+    'secret_key' => getenv('SAJARI_SECRET_KEY'),
+    'company'    => getenv('SAJARI_COMPANY'),
+    'collection' => getenv('SAJARI_COLLECTION'),
+));
+
+try {
+    $results = $c->search(array(
+        'q'          => getenv('QUERY'),
+        'maxresults' => 10,
+    ));
+    echo var_export($results, true), PHP_EOL;
+} catch (EngineException $e) {
+    echo "There was an error running the search. ", $e->getMessage(), PHP_EOL;
+}
