f14cd1a0c3ece059ae0b0aa50e5fe047fb4f76a9
Trent Billington <pudgepacket@gmail.com>
2016-12-13 09:36:46 +1100

    FieldFilter: use same constructor as Go

--- src/Sajari/Search/FieldFilter.php
+++ src/Sajari/Search/FieldFilter.php
@@ -2,10 +2,6 @@
 
 namespace Sajari\Search;
 
-// require_once __DIR__.'/../proto/value.php';
-// require_once __DIR__.'/../proto/doc.php';
-// require_once __DIR__.'/../proto/query.php';
-
 require_once __DIR__.'/../proto/engine/value.php';
 require_once __DIR__.'/../proto/engine/query/v1/query.php';
 
@@ -14,33 +10,62 @@ use sajari\engine\query\v1\Filter as EngineFilter;
 use sajari\engine\Value;
 use sajari\engine\Value\Repeated;
 
+use sajari\engine\query\v1\Filter\Field\Operator;
+
 class FieldFilter extends Filter
 {
-    const EQUAL_TO = 0;
-    const DOES_NOT_EQUAL = 1;
-    const GREATER_THAN = 2;
-    const GREATER_THAN_OR_EQUAL_TO = 3;
-    const LESS_THAN = 4;
-    const LESS_THAN_OR_EQUAL_TO = 5;
-    const CONTAINS = 6;
-    const DOES_NOT_CONTAIN = 7;
-    const ENDS_WITH = 8;
-    const STARTS_WITH = 9;
     /** @var integer $operator */
     private $operator;
+
     /** @var string $field */
     private $field;
+
+    /** @var mixed $value */
     private $value;
 
     /**
      * FieldFilter constructor.
-     * @param int $operator
      * @param string $field
+     * @param string $operator
      * @param $value
      */
-    public function __construct($operator, $field, $value)
+    public function __construct($field, $operator, $value)
     {
-        $this->operator = $operator;
+        switch ($operator) {
+            case "=":
+                $this->operator = Operator::EQUAL_TO;
+                break;
+            case "!=":
+                $this->operator = Operator::NOT_EQUAL_TO;
+                break;
+            case ">":
+                $this->operator = Operator::GREATER_THAN;
+                break;
+            case ">=":
+                $this->operator = Operator::GREATER_THAN_OR_EQUAL_TO;
+                break;
+            case "<":
+                $this->operator = Operator::LESS_THAN;
+                break;
+            case "<=":
+                $this->operator = Operator::LESS_THAN_OR_EQUAL_TO;
+                break;
+            case "~":
+                $this->operator = Operator::CONTAINS;
+                break;
+            case "!~":
+                $this->operator = Operator::DOES_NOT_CONTAIN;
+                break;
+            case "$":
+                $this->operator = Operator::HAS_SUFFIX;
+                break;
+            case "^":
+                $this->operator = Operator::HAS_PREFIX;
+                break;
+
+            default:
+                throw new \Exception(sprintf("invalid field filter operator: %s", $operator), 1);
+        }
         $this->field = $field;
         $this->value = $value;
     }
@@ -67,105 +92,6 @@ class FieldFilter extends Filter
     public function getValue()
     {
         return $this->value;
-    }
-
-    /**
-     * @param string $field
-     * @param $value
-     * @return FieldFilter
-     */
-    public static function EqualTo($field, $value)
-    {
-        return new FieldFilter(FieldFilter::EQUAL_TO, $field, $value);
-    }
-
-    /**
-     * @param string $field
-     * @param $value
-     * @return FieldFilter
-     */
-    public static function DoesNotEqual($field, $value)
-    {
-        return new FieldFilter(FieldFilter::DOES_NOT_EQUAL, $field, $value);
-    }
-
-    /**
-     * @param string $field
-     * @param $value
-     * @return FieldFilter
-     */
-    public static function GreaterThan($field, $value)
-    {
-        return new FieldFilter(FieldFilter::GREATER_THAN, $field, $value);
-    }
-    /**
-     * @param string $field
-     * @param $value
-     * @return FieldFilter
-     */
-    public static function GreaterThanOrEqualTo($field, $value)
-    {
-        return new FieldFilter(FieldFilter::GREATER_THAN_OR_EQUAL_TO, $field, $value);
-    }
-
-    /**
-     * @param string $field
-     * @param $value
-     * @return FieldFilter
-     */
-    public static function LessThan($field, $value)
-    {
-        return new FieldFilter(FieldFilter::LESS_THAN, $field, $value);
-    }
-
-    /**
-     * @param string $field
-     * @param $value
-     * @return FieldFilter
-     */
-    public static function LessThanOrEqualTo($field, $value)
-    {
-        return new FieldFilter(FieldFilter::LESS_THAN_OR_EQUAL_TO, $field, $value);
-    }
-
-    /**
-     * @param string $field
-     * @param $value
-     * @return FieldFilter
-     */
-    public static function Contains($field, $value)
-    {
-        return new FieldFilter(FieldFilter::CONTAINS, $field, $value);
-    }
-
-    /**
-     * @param string $field
-     * @param $value
-     * @return FieldFilter
-     */
-    public static function DoesNotContain($field, $value)
-    {
-        return new FieldFilter(FieldFilter::DOES_NOT_CONTAIN, $field, $value);
-    }
-
-    /**
-     * @param string $field
-     * @param $value
-     * @return FieldFilter
-     */
-    public static function EndsWith($field, $value)
-    {
-        return new FieldFilter(FieldFilter::ENDS_WITH, $field, $value);
-    }
-
-    /**
-     * @param string $field
-     * @param $value
-     * @return FieldFilter
-     */
-    public static function StartsWith($field, $value)
-    {
-        return new FieldFilter(FieldFilter::STARTS_WITH, $field, $value);
     }
 
     /**
