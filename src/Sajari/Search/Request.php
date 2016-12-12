b7a2ca78b3783bc7598ffb122b6b8f2cdb51d2da
Trent Billington <pudgepacket@gmail.com>
2016-12-13 08:12:19 +1100

    Progress on Client and Request moving to new proto

--- src/Sajari/Search/Request.php
+++ src/Sajari/Search/Request.php
@@ -2,34 +2,161 @@
 
 namespace Sajari\Search;
 
-require_once __DIR__.'/../proto/doc.php';
-require_once __DIR__.'/../proto/query.php';
+require_once __DIR__.'/../proto/api/query/v1/query.php';
+require_once __DIR__.'/../proto/engine/query/v1/query.php';
 
-// use sajari\engine\query\
-use sajari\engine\query\SearchRequest as ProtoSearchRequest;
+use sajari\api\query\v1\SearchRequest as ApiSearchRequest;
+use sajari\api\query\v1\SearchRequest\Tracking as ApiTracking;
+
+use sajari\engine\query\v1\SearchRequest as EngineSearchRequest;
+
+use Sajari\Search\Tracking;
+use Sajari\Search\IndexQuery;
+use Sajari\Search\FeatureQuery;
 
 class Request
 {
-    /** @var string $body */
-    private $body;
-    /** @var int $resultsPerPage */
-    private $resultsPerPage;
+    /** @var Tracking $tracking */
+    private $tracking;
+
     /** @var Filter $filter */
     private $filter;
-    /** @var int $page */
-    private $page;
-    /** @var WeightedBody $weightedBody */
-    private $weightedBody;
-    /** @var string[] $fields */
-    private $fields;
-    /** @var InstanceBoost[] $instanceBoosts */
-    private $instanceBoosts;
-    /** @var Aggregate[] $aggregates */
-    private $aggregates;
+
+    /** @var IndexQuery $indexQuery */
+    private $indexQuery;
+
+    /** @var FeatureQuery $featureQuery */
+    private $featureQuery;
+
+    /** @var integer $offset */
+    private $offset;
+
+    /** @var integer $limit */
+    private $limit;
+
     /** @var Sort[] $sorts */
     private $sorts;
-    /** @var FieldBoost[] $fieldBoosts */
-    private $fieldBoosts;
+
+    /** @var string[] $fields */
+    private $fields;
+
+    /** @var Aggregate[] $aggregates */
+    private $aggregates;
+
+    /** @var Transform[] $transforms */
+    private $transforms;
+
+    /**
+     * @return Tracking
+     */
+    public function getTracking()
+    {
+        return $this->tracking;
+    }
+
+    /**
+     * @param Tracking $tracking
+     */
+    public function setTracking($tracking)
+    {
+        $this->tracking = $tracking;
+    }
+
+    /**
+     * @return Filter
+     */
+    public function getFilter()
+    {
+        return $this->filter;
+    }
+
+    /**
+     * @param Filter $filter
+     */
+    public function setFilter($filter)
+    {
+        $this->filter = $filter;
+    }
+
+    /**
+     * @return IndexQuery
+     */
+    public function getIndexQuery()
+    {
+        return $this->indexQuery;
+    }
+
+    /**
+     * @param IndexQuery $indexQuery
+     */
+    public function setIndexQuery($indexQuery)
+    {
+        $this->indexQuery = $indexQuery;
+    }
+
+    /**
+     * @return FeatureQuery
+     */
+    public function getFeatureQuery()
+    {
+        return $this->featureQuery;
+    }
+
+    /**
+     * @param FeatureQuery $featureQuery
+     */
+    public function setFeatureQuery($featureQuery)
+    {
+        $this->featureQuery = $featureQuery;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getOffset()
+    {
+        return $this->offset;
+    }
+
+    /**
+     * @param integer $offset
+     */
+    public function setOffset($offset)
+    {
+        $this->offset = $offset;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getLimit()
+    {
+        return $this->limit;
+    }
+
+    /**
+     * @param integer $limit
+     */
+    public function setLimit($limit)
+    {
+        $this->limit = $limit;
+    }
+
+    /**
+     * @return Sort[]
+     */
+    public function getSorts()
+    {
+        return $this->sorts;
+    }
+
+    /**
+     * @param Sort[] $sorts
+     */
+    public function setSorts(array $sorts)
+    {
+        $this->sorts = $sorts;
+    }
 
     /**
      * @return FieldBoost[]
@@ -64,54 +191,6 @@ class Request
     }
 
     /**
-     * @return Aggregate[]
-     */
-    public function getAggregates()
-    {
-        return $this->aggregates;
-    }
-
-    /**
-     * @param Aggregate[] $aggregates
-     */
-    public function setAggregates(array $aggregates)
-    {
-        $this->aggregates = $aggregates;
-    }
-
-    /**
-     * @return Sort[]
-     */
-    public function getSorts()
-    {
-        return $this->sorts;
-    }
-
-    /**
-     * @param Sort[] $sorts
-     */
-    public function setSorts(array $sorts)
-    {
-        $this->sorts = $sorts;
-    }
-
-    /**
-     * @return WeightedBody
-     */
-    public function getWeightedBody()
-    {
-        return $this->weightedBody;
-    }
-
-    /**
-     * @param WeightedBody $weightedBody
-     */
-    public function setWeightedBody($weightedBody)
-    {
-        $this->weightedBody = $weightedBody;
-    }
-
-    /**
      * @return \string[]
      */
     public function getFields()
@@ -128,135 +207,108 @@ class Request
     }
 
     /**
-     * @return mixed
+     * @return Aggregate[]
      */
-    public function getPage()
+    public function getAggregates()
     {
-        return $this->page;
+        return $this->aggregates;
     }
 
     /**
-     * @param mixed $page
+     * @param Aggregate[] $aggregates
      */
-    public function setPage($page)
+    public function setAggregates(array $aggregates)
     {
-        $this->page = $page;
+        $this->aggregates = $aggregates;
     }
 
     /**
-     * @return Filter
+     * @return Transform[]
      */
-    public function getFilter()
+    public function getTransforms()
     {
-        return $this->filter;
+        return $this->transforms;
     }
 
     /**
-     * @param Filter $filter
+     * @param Transform[] $transforms
      */
-    public function setFilter($filter)
+    public function setTransforms(array $transforms)
     {
-        $this->filter = $filter;
+        $this->transforms = $transforms;
     }
 
     /**
-     * @return string
-     */
-    public function getBody()
-    {
-        return $this->body;
-    }
-
-    /**
-     * @param string $body
-     */
-    public function setBody($body)
-    {
-        $this->body = $body;
-    }
-
-    /**
-     * @return integer
-     */
-    public function getMaxResults()
-    {
-        return $this->maxResults;
-    }
-
-    /**
-     * @param integer $resultsPerPage
-     */
-    public function setResultsPerPage($resultsPerPage)
-    {
-        $this->resultsPerPage = $resultsPerPage;
-    }
-
-    /**
-     * @return ProtoSearchRequest
+     * @return ApiSearchRequest
      * @throws Exception
      */
     public function Proto()
     {
-        $r = new ProtoSearchRequest();
+        $er = new EngineSearchRequest();
 
-        // Page
-        if (isset($this->page)) {
-            $r->setPage($this->page);
+        // Offset
+        if (isset($this->offset)) {
+            $er->setOffset($this->offset);
         }
 
-        // Body
-        if (isset($this->body)) {
-            $r->setBody($this->body->Proto());
+        // Limit
+        if (isset($this->limit)) {
+            $er->setLimit($this->limit);
         }
 
-        // Max Results
-        if (isset($this->resultsPerPage)) {
-            $r->setResultsPerPage($this->resultsPerPage);
+        // IndexQuery
+        if (isset($this->indexQuery))
+        {
+            $er->setIndexQuery($this->indexQuery->Proto());
+        }
+
+        // FeatureQuery
+        if (isset($this->featureQuery))
+        {
+            $er->setFeatureQuery($this->featureQuery->Proto());
         }
 
         // Filter
         if (isset($this->filter)) {
-            $r->setFilter($this->filter->Proto());
-        }
-
-        // Fields
-        if (isset($this->fields)) {
-            foreach ($this->fields as $field) {
-                $r->addFields($field);
-            }
-        }
-
-        // Weighted Body
-        if (isset($this->weightedBody)) {
-            $r->setWeightedBody($this->weightedBody->Proto());
+            $er->setFilter($this->filter->Proto());
         }
 
         // Sorts
         if (isset($this->sorts)) {
             foreach ($this->sorts as $s) {
-                $r->addSort($s->Proto());
+                $er->addSort($s->Proto());
             }
+        }
+
+        // Fields
+        if (isset($this->fields))
+        {
+            $er->setFields($this->fields);
         }
 
         // Aggregates
         if (isset($this->aggregates)) {
             foreach ($this->aggregates as $agg) {
-                $r->addAggregates($agg->Proto());
+                $er->addAggregates($agg->Proto());
             }
         }
 
-        // Instance Boosts
-        if (isset($this->instanceBoosts)) {
-            foreach ($this->instanceBoosts as $ib) {
-                $r->addInstanceBoosts($ib->Proto());
+        // Transforms
+        if (isset($this->transforms))
+        {
+            foreach ($this->transforms as $t) {
+                $er->addTransforms($t->Proto());
             }
         }
 
-        // Field Boosts
-        if (isset($this->fieldBoosts)) {
-            foreach ($this->fieldBoosts as $mb) {
-                $r->addFieldBoosts($mb->Proto());
-            }
+        $r = new ApiSearchRequest();
+
+        $r->setSearchRequest($er);
+
+        // Tracking
+        if (isset($this->tracking))
+        {
+            $r->setTracking($this->tracking->Proto());
         }
 
         return $r;
