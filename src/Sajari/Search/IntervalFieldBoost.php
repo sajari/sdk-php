5810a4068dfa879ae9b73cfba0bd137c4f47cb1a
Trent Billington <pudgepacket@gmail.com>
2016-12-14 13:00:33 +1100

    Update IntervalFieldBoost, split IntervalFieldBoostPoint into separate file

--- src/Sajari/Search/IntervalFieldBoost.php
+++ src/Sajari/Search/IntervalFieldBoost.php
@@ -2,12 +2,12 @@
 
 namespace Sajari\Search;
 
-require_once __DIR__.'/../proto/doc.php';
-require_once __DIR__.'/../proto/query.php';
+require_once __DIR__.'/../proto/engine/query/v1/query.php';
 
-use sajari\engine\query\FieldBoost\Interval as ProtoInterval;
-use sajari\engine\query\FieldBoost as ProtoFieldBoost;
-use sajari\engine\query\FieldBoost\Interval\Point as ProtoPoint;
+use sajari\engine\query\v1\FieldBoost as EngineFieldBoost;
+use sajari\engine\query\v1\FieldBoost\Interval as EngineInterval;
+
+use Sajari\Search\IntervalFieldBoostPoint;
 
 class IntervalFieldBoost extends FieldBoost
 {
@@ -56,57 +56,14 @@ class IntervalFieldBoost extends FieldBoost
 
     public function Proto()
     {
-        $imb = new ProtoInterval();
+        $imb = new EngineInterval();
         $imb->setField($this->field);
         foreach ($this->points as $point) {
             $imb->addPoints($point->Proto());
         }
 
-        $mb = new ProtoFieldBoost();
+        $mb = new EngineFieldBoost();
         $mb->setInterval($imb);
         return $mb;
-    }
-}
-
-class IntervalFieldBoostPoint
-{
-    /** @var float $point */
-    private $point;
-    /** @var float $value */
-    private $value;
-
-    /**
-     * IntervalFieldBoostPoint constructor.
-     * @param float $point
-     * @param float $value
-     */
-    public function __construct($point, $value)
-    {
-        $this->point = $point;
-        $this->value = $value;
-    }
-
-    /**
-     * @return float
-     */
-    public function getPoint()
-    {
-        return $this->point;
-    }
-
-    /**
-     * @return float
-     */
-    public function getValue()
-    {
-        return $this->value;
-    }
-
-    public function Proto()
-    {
-        $p = new ProtoPoint();
-        $p->setPoint($this->point);
-        $p->setValue($this->value);
-        return $p;
     }
 }
