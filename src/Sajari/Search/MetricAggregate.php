6c31b011f1a66eab3f790689ce076ddbe341a94d
Trent Billington <pudgepacket@gmail.com>
2016-12-14 13:03:26 +1100

    Update MetricAggregate

--- src/Sajari/Search/MetricAggregate.php
+++ src/Sajari/Search/MetricAggregate.php
@@ -2,23 +2,21 @@
 
 namespace Sajari\Search;
 
-require_once __DIR__.'/../proto/doc.php';
-require_once __DIR__.'/../proto/query.php';
+require_once __DIR__.'/../proto/engine/query/v1/query.php';
 
-use sajari\engine\query\Request\AggregatesEntry as ProtoAggregatesEntry;
-use sajari\engine\query\Aggregate\Metric as ProtoMetric;
-use sajari\engine\query\Aggregate as ProtoAggregate;
+use sajari\engine\query\v1\Aggregate as EngineAggregate;
+use sajari\engine\query\v1\Aggregate\Metric as EngineMetric;
+use sajari\engine\query\v1\Aggregate\Metric\Type;
+use sajari\engine\query\v1\Request\AggregatesEntry as EngineAggregatesEntry;
 
 class MetricAggregate extends Aggregate
 {
-    const AVG = 0;
-    const MIN = 1;
-    const MAX = 2;
-    const SUM = 3;
     /** @var string $field */
     private $field;
+
     /** @var int $type */
     private $type;
+
     /** @var string $name */
     private $name;
 
@@ -61,34 +59,34 @@ class MetricAggregate extends Aggregate
 
     public static function Avg($field, $name)
     {
-        return new MetricAggregate($field, MetricAggregate::AVG, $name);
+        return new MetricAggregate($field, Type::AVG, $name);
     }
 
     public static function Min($field, $name)
     {
-        return new MetricAggregate($field, MetricAggregate::MIN, $name);
+        return new MetricAggregate($field, Type::MIN, $name);
     }
 
     public static function Max($field, $name)
     {
-        return new MetricAggregate($field, MetricAggregate::MAX, $name);
+        return new MetricAggregate($field, Type::MAX, $name);
     }
 
     public static function Sum($field, $name)
     {
-        return new MetricAggregate($field, MetricAggregate::SUM, $name);
+        return new MetricAggregate($field, Type::SUM, $name);
     }
 
     public function Proto()
     {
-        $ae = new ProtoAggregatesEntry();
+        $ae = new EngineAggregatesEntry();
         $ae->setKey($this->name);
 
-        $ma = new ProtoMetric();
+        $ma = new EngineMetric();
         $ma->setField($this->field);
         $ma->setType($this->type);
 
-        $a = new ProtoAggregate();
+        $a = new EngineAggregate();
         $a->setMetric($ma);
 
         $ae->setValue($a);
