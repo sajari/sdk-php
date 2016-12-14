1f81fa69638eefb301e07d2aeeaace7522d15ca0
Trent Billington <pudgepacket@gmail.com>
2016-12-14 14:33:19 +1100

    Update Sort

--- src/Sajari/Search/Sort.php
+++ src/Sajari/Search/Sort.php
@@ -2,24 +2,23 @@
 
 namespace Sajari\Search;
 
-require_once __DIR__.'/../proto/doc.php';
-require_once __DIR__.'/../proto/query.php';
+require_once __DIR__.'/../proto/engine/query/v1/query.php';
 
-use sajari\engine\query\Sort as ProtoSort;
+use sajari\engine\query\v1\Sort as EngineSort;
+use sajari\engine\query\v1\Sort\Order;
 
 class Sort
 {
-    const ASC = 0;
-    const DESC = 1;
     /** @var string $field */
     private $field;
+
     /** @var int $order */
     private $order;
 
     /**
      * Sort constructor.
      * @param string $field
-     * @param $order
+     * @param int $order
      */
     public function __construct($field, $order)
     {
@@ -49,7 +48,7 @@ class Sort
      */
     public static function Asc($field)
     {
-        return new Sort($field, Sort::ASC);
+        return new Sort($field, Order::ASC);
     }
 
     /**
@@ -58,15 +57,15 @@ class Sort
      */
     public static function Desc($field)
     {
-        return new Sort($field, Sort::DESC);
+        return new Sort($field, Order::DESC);
     }
 
     /**
-     * @return engine\query\Sort
+     * @return EngineSort
      */
     public function Proto()
     {
-        $s = new ProtoSort();
+        $s = new EngineSort();
         $s->setField($this->field);
         $s->setOrder($this->order);
         return $s;
