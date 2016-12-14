a563b02bcd549341b17db572244c0d2fe1be9f1e
Trent Billington <pudgepacket@gmail.com>
2016-12-14 12:10:11 +1100

    Updating FeatureFieldBoost, FeatureQuery, and FilterFieldBoost

---
+++ src/Sajari/Search/FeatureFieldBoost.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Sajari\Search;
+
+require_once __DIR__.'/../proto/engine/query/v1/query.php';
+
+use sajari\engine\query\v1\SearchRequest\FeatureQuery\FieldBoost as EngineFieldBoost;
+use Sajari\Search\FieldBoost;
+
+class FeatureFieldBoost
+{
+    /**
+     * @var FieldBoost $fieldBoost
+     */
+    private $fieldBoost;
+
+    /**
+     * @var float $value
+     */
+    private $value;
+
+    /**
+     * @return FeatureFieldBoost
+     */
+    public function __construct($fieldBoost, $value)
+    {
+        $this->fieldBoost = $fieldBoost;
+        $this->value = $value;
+    }
+
+    /**
+     * @return EngineFieldBoost
+     */
+    public function Proto()
+    {
+        $fb = new EngineFieldBoost();
+
+        $fb->setFieldBoost($this->fieldBoost->Proto());
+        $fb->setValue($this->value);
+
+        return $fb;
+    }
+}
--- src/Sajari/Search/FeatureQuery.php
+++ src/Sajari/Search/FeatureQuery.php
@@ -5,9 +5,18 @@ namespace Sajari\Search;
 require_once __DIR__.'/../proto/engine/query/v1/query.php';
 
 use sajari\engine\query\v1\SearchRequest\FeatureQuery as EngineFeatureQuery;
+use Sajari\Search\FeatureFieldBoost;
 
 class FeatureQuery
 {
+    /** @var FeatureFieldBoost[] $fieldBoosts */
+    private $fieldBoosts;
+
+    public function setFieldBoosts(array $fieldBoosts)
+    {
+        $this->fieldBoosts = $fieldBoosts;
+    }
+
     /**
      * Proto returns the proto representation of a FeatureQuery
      *
@@ -16,6 +25,12 @@ class FeatureQuery
     public function Proto()
     {
         $fq = new EngineFeatureQuery();
+
+        if (isset($this->fieldBoosts)) {
+            foreach ($this->fieldBoosts as $b) {
+                $fq->addFieldBoosts($b->Proto());
+            }
+        }
 
         return $fq;
     }
