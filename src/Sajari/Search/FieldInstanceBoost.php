f93220ad4f19eab7d4bed955b039b6f779839e13
Trent Billington <pudgepacket@gmail.com>
2016-12-14 12:45:49 +1100

    Update FieldInstanceBoost

--- src/Sajari/Search/FieldInstanceBoost.php
+++ src/Sajari/Search/FieldInstanceBoost.php
@@ -2,11 +2,10 @@
 
 namespace Sajari\Search;
 
-require_once __DIR__.'/../proto/doc.php';
-require_once __DIR__.'/../proto/query.php';
+require_once __DIR__.'/../proto/engine/query/v1/query.php';
 
-use sajari\engine\query\InstanceBoost\Field as ProtoField;
-use sajari\engine\query\InstanceBoost as ProtoInstanceBoost;
+use sajari\engine\query\v1\InstanceBoost\Field as EngineField;
+use sajari\engine\query\v1\InstanceBoost as EngineInstanceBoost;
 
 class FieldInstanceBoost extends InstanceBoost
 {
@@ -44,11 +43,11 @@ class FieldInstanceBoost extends InstanceBoost
 
     public function Proto()
     {
-        $f = new ProtoField();
+        $f = new EngineField();
         $f->setField($this->field);
         $f->setValue($this->value);
 
-        $ib = new ProtoInstanceBoost();
+        $ib = new EngineInstanceBoost();
         $ib->setField($f);
 
         return $ib;
