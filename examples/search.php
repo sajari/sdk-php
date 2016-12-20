d03e3e78e0fb41330711c8d3b94e34b3dd231747
Trent Billington <pudgepacket@gmail.com>
2016-12-20 17:24:42 +1100

    Update Search example

--- examples/search.php
+++ examples/search.php
@@ -1,101 +1,59 @@
 <?php
 
-require  dirname(__FILE__) . '/../src/sajari.php';
+require  __DIR__ . "/vendor/autoload.php";
 
-use Sajari\WithEndpoint;
-use Sajari\Client;
-use Sajari\Request;
+// Get config from environment
+$project = getenv("SJ_PROJECT");
+$collection = getenv("SJ_COLLECTION");
+$key_id = getenv("SJ_KEY_ID");
+$key_secret = getenv("SJ_KEY_SECRET");
 
-$opts = [new WithEndpoint('server_address')];
-
-$c = new Client('myproject', 'mycollection', $opts);
-
-$r = new Request();
-$r->setBody("request body of words to search for");
-$r->setMaxResults(10);
-$r->setPage(1);
-$r->setFilter(
-    Sajari\All([
-        Sajari\EqualTo("type", "webpage"),
-        Sajari\EqualTo("category", "education"),
-    ])
+// Create a client with the default configuration
+$client = \Sajari\Client\Client::DefaultClient(
+    $project,
+    $collection,
+    [new \Sajari\Client\WithAuth($key_id, $key_secret)]
 );
-$r->setFields([
-    "url", "title", "description", "popularity"
-]);
-$r->setAggregates([
-    Sajari\SumMetricAggregate("float", "float-sum"),
-    Sajari\AvgMetricAggregate("float", "float-avg"),
-    Sajari\MaxMetricAggregate("float", "float-max"),
-    Sajari\MinMetricAggregate("float", "float-min"),
 
-    Sajari\Bucket(
-        "more-than-one",
-        [Sajari\BucketEntry(
-            "more-than-one",
-            Sajari\GreaterThan("float", 1.0)
-        )]
-    ),
-    Sajari\Bucket(
-        "more-than-one-or-one",
-        [Sajari\BucketEntry(
-            "more-than-one-or-equal",
-            Sajari\GreaterThanOrEqualTo("float", 1.0)
-        )]
-    ),
-    Sajari\Bucket(
-        "less-than-fifty",
-        [Sajari\BucketEntry(
-            "less-than-fifty",
-            Sajari\LessThan("float", 50.0)
-        )]
-    ),
-    Sajari\Bucket(
-        "equal-to-twenty",
-        [Sajari\BucketEntry(
-            "equal-to-twenty",
-            Sajari\EqualTo("float", 20.0)
-        )]
-    ),
-    Sajari\Count("float", "how-many")
-]);
-$r->setIndexBoosts([
-    // Multiply the weighting of the title by 2
-    Sajari\FieldBoost("title", 2)
-]);
+// Create request object to perform out query with
+$request = new \Sajari\Search\Request();
+// Set limit to determine how many results to request
+$request->setLimit(1);
+// Use default tracking to state that we're not requesting tracking tokens
+$request->setTracking(new \Sajari\Search\Tracking());
 
-$sydLat = -33.861662;
-$sydLng = 151.210799;
-
-$timeNow = time();
-$yearAgo = time() - (365 * 24 * 60 * 60);
-$decadeAgo = time() - (10 * 365 * 24 * 60 * 60);
-
-$r->setMetaBoosts([
-    // Boost results that are in english
-    new Sajari\FilterMetaBoost(
-        Sajari\EqualTo("language", "en"), 2
-    ),
-
-    // Boost results in Sydney
-    Sajari\BoostInsideRegion("lat", "lng", $sydLat, $sydLng, 0.1, 2),
-
-    // Boost more recent results, decreasing with age
-    // Results from today get 2x score
-    // Results from a year ago get 1x score
-    // Results from 10 years ago get 0x score
-    new Sajari\IntervalMetaBoost("pageCreated", [
-        new Sajari\IntervalMetaBoostPoint($timeNow, 2),
-        new Sajari\IntervalMetaBoostPoint($yearAgo, 1),
-        new Sajari\IntervalMetaBoostPoint($decadeAgo, 0),
-    ]),
-]);
-
+// Perform a search request
 try {
-    $res = $c->Search($r);
-} catch (Exception $e) {
-    echo 'Caught exception: ', $e->getMessage();
+    $result = $client->Search($request);
+} catch (\Sajari\Error\MalformedRequestException $e) {
+    printf("malformed request: %s\n", $e->getMessage());
+    exit(1);
+} catch (\Sajari\Error\PermissionDeniedException $e) {
+    printf("%s\n", $e->getMessage());
+    exit(1);
+} catch (\Sajari\Error\Unavailable $e) {
+    printf("%s\n", $e->getMessage());
+    // Retry ?
+    exit(1);
+} catch (\Sajari\Error\Base $e) {
+    printf("%s\n", $e->getMessage());
+    exit(1);
+} catch (\Exception $e) {
+    echo "Caught exception (", $e->getCode(), "): ", $e->getMessage(), "\n";
     exit(1);
 }
 
-print_r($res);
+printf("Search found %u results in %s.\n", $result->getTotalResults(), $result->getTime());
+foreach ($result->getResults() as $r) {
+    printf("Score: %f %f\n", $r->getScore(), $r->getIndexScore());
+    foreach ($r->getValues() as $v) {
+        if (is_array($v->getValue())) {
+            printf("   %s:\n", $v->getKey());
+            foreach ($v->getValue() as $i) {
+                printf("      %s\n", $i);
+            }
+        } else {
+            printf("   %s: %s\n", $v->getKey(), $v->getValue());
+        }
+    }
+}
