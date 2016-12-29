f74d5d451313c52dd81cf0b9c0f9896f717c57dd
Trent Billington <pudgepacket@gmail.com>
2016-12-29 12:14:28 +1100

    Add test bootstrapper, add Client and Value tests

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__.'/src/Sajari/proto/engine/value.php';
+require_once __DIR__.'/src/Sajari/proto/engine/key.php';
+require_once __DIR__.'/src/Sajari/proto/engine/status.php';
+require_once __DIR__.'/src/Sajari/proto/engine/store/record/record.php';
+require_once __DIR__.'/src/Sajari/proto/api/query/v1/query.php';
+
+require 'vendor/autoload.php';
---
+++ tests/Sajari/Record/ValueTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Sajari\Client;
+
+class TestValue
+{
+    public $original;
+    public $string;
+    public $proto;
+
+    public function __construct($v)
+    {
+        $pv = new \sajari\engine\Value();
+        if (is_array($v)) {
+            $repeated = new \sajari\engine\Value\Repeated();
+            foreach ($v as $x) {
+                if ($v instanceof \DateTime) {
+                    $repeated->addValues($x->format(\DateTime::ATOM));
+                } else {
+                    $repeated->addValues(sprintf("%s", $x));
+                }
+            }
+            $pv->setRepeated($repeated);
+        } else if (is_null($v)) {
+            $pv->setNull(true);
+        } else {
+            if ($v instanceof \DateTime) {
+                $pv->setSingle($v->format(\DateTime::ATOM));
+            } else {
+                $pv->setSingle(sprintf("%s", $v));
+            }
+        }
+
+        $stringOfValue = null;
+        if (is_array($v)) {
+            $arrayOfString = [];
+            foreach ($v as $x) {
+                if ($x instanceof \DateTime) {
+                    $arrayOfString[] = $x->format(\DateTime::ATOM);
+                } else {
+                    $arrayOfString[] = sprintf("%s", $x);
+                }
+            }
+            $stringOfValue = $arrayOfString;
+        } else {
+            if ($v instanceof \DateTime) {
+                $stringOfValue = $v->format(\DateTime::ATOM);
+            } else {
+                $stringOfValue = sprintf("%s", $v);
+            }
+        }
+
+        $this->original = $v;
+        $this->string = $stringOfValue;
+        $this->proto = $pv;
+    }
+}
+
+class ValueTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param $v
+     * @return TestValue
+     */
+    public static function makeProtoValue($v)
+    {
+        return new TestValue($v);
+    }
+
+    /** @var TestValue[] $protoValueTests */
+    public $protoValueTests;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->protoValueTests = array_map(
+            "self::makeProtoValue",
+            [
+                "",
+                "5",
+                "dog",
+                "12.34",
+                [],
+                [1],
+                [1, 2, 3],
+                ["test"],
+                [(new \DateTime("2015-11-01 00:00:00"))->format(\DateTime::ATOM)],
+                1,
+                1.25,
+                true,
+                false,
+                (new \DateTime("2015-11-01 00:00:00"))->format(\DateTime::ATOM)
+            ]
+        );
+    }
+
+    public function testValueToProto()
+    {
+        foreach ($this->protoValueTests as $test) {
+            $this->assertEquals(
+                \Sajari\Record\Value::ToProto($test->original),
+                $test->proto
+            );
+        }
+    }
+
+    public function testValueFromProto()
+    {
+        foreach ($this->protoValueTests as $test) {
+            $this->assertSame(
+                \Sajari\Record\Value::FromProto($test->proto),
+                $test->string
+            );
+        }
+    }
+
+    public function testValueToProtoAndBack()
+    {
+        foreach ($this->protoValueTests as $test) {
+            $this->assertSame(
+                \Sajari\Record\Value::FromProto(
+                    \Sajari\Record\Value::ToProto($test->original)
+                ),
+                $test->string
+            );
+        }
+    }
+}
