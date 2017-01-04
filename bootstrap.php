d8666706a7dab798ce858694a03bbc11d42645eb
Trent Billington <pudgepacket@gmail.com>
2017-01-04 11:36:08 +1100

    Add tests for Body

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/src/Sajari/proto/engine/value.php';
 require_once __DIR__.'/src/Sajari/proto/engine/key.php';
 require_once __DIR__.'/src/Sajari/proto/engine/status.php';
 require_once __DIR__.'/src/Sajari/proto/engine/store/record/record.php';
+require_once __DIR__.'/src/Sajari/proto/engine/query/v1/query.php';
 require_once __DIR__.'/src/Sajari/proto/api/query/v1/query.php';
 
 require 'vendor/autoload.php';
---
+++ tests/Sajari/Search/BodyTest.php
@@ -0,0 +1,37 @@
+<?php
+
+class BodyTest extends \PHPUnit_Framework_TestCase
+{
+    private $testValues;
+
+    public static function protoBody($values)
+    {
+        list($text, $weight) = $values;
+        $pb = new \sajari\engine\query\v1\Body();
+        $pb->setText($text);
+        $pb->setWeight($weight);
+        return [$values, $pb];
+    }
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->testValues = array_map(
+            "self::protoBody",
+            [
+                ["test", 1.0]
+            ]
+        );
+    }
+
+    public function testToProto()
+    {
+        foreach ($this->testValues as $testValue) {
+            list(list($text, $weight), $protoValue) = $testValue;
+            $this->assertEquals(
+                $protoValue,
+                (new \Sajari\Search\Body($text, $weight))->ToProto()
+            );
+        }
+    }
+}
