ec336147c45f3b8960d71637613c608ff378b07d
Trent Billington <pudgepacket@gmail.com>
2017-01-12 17:01:52 +1100

    Updated the files that are required by the tests

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,12 @@
 <?php
 
-require_once __DIR__.'/src/Sajari/proto/engine/value.php';
+require_once __DIR__.'/src/Sajari/proto/api/query/v1/query.php';
+require_once __DIR__.'/src/Sajari/proto/engine/query/v1/query.php';
+require_once __DIR__.'/src/Sajari/proto/engine/schema/schema.php';
+require_once __DIR__.'/src/Sajari/proto/engine/store/record/record.php';
+require_once __DIR__.'/src/Sajari/proto/engine/empty.php';
 require_once __DIR__.'/src/Sajari/proto/engine/key.php';
 require_once __DIR__.'/src/Sajari/proto/engine/status.php';
-require_once __DIR__.'/src/Sajari/proto/engine/store/record/record.php';
-require_once __DIR__.'/src/Sajari/proto/engine/query/v1/query.php';
-require_once __DIR__.'/src/Sajari/proto/api/query/v1/query.php';
+require_once __DIR__.'/src/Sajari/proto/engine/value.php';
 
 require 'vendor/autoload.php';
