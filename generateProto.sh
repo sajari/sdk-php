2af3c7f42bcb959cfaf7498ef1ee00257c0b8a06
Trent Billington <pudgepacket@gmail.com>
2017-05-12 15:54:11 +1000

    update generate script to work with protoc 3.3.0

--- generateProto.sh
+++ generateProto.sh
@@ -9,6 +9,10 @@ if [ -z "$PROTO_SRC" ]; then
   die "must set PROTO_SRC"
 fi
 
+if [ -z "$GRPC_PHP_PLUGIN" ]; then
+  die "must set GRPC_PHP_PLUGIN"
+fi
+
 echo 1>&2 "Checking required tools:"
 for tool in protoc; do
   q=$(which $tool) || die "didn't find $tool"
@@ -17,12 +21,12 @@ done
 
 dest="src/Sajari/proto"
 
-rm -fr $dest
+rm -r $dest
 mkdir -p $dest
 
 echo "$dest"
 echo 1>&2 "Building protos:"
 for dir in $(find $PROTO_SRC -name '*.proto' | xargs -n1 dirname | sort | uniq); do
   echo 1>&2 "- $dir"
-  protoc -I$PROTO_SRC --php_out=$dest --grpc_out=$dest --plugin=protoc-gen-grpc=/Users/trent/grpc/bins/opt/grpc_php_plugin $dir/*.proto
+  protoc -I$PROTO_SRC --php_out=$dest --grpc_out=$dest --plugin=protoc-gen-grpc=$GRPC_PHP_PLUGIN $dir/*.proto
 done
