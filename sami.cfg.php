70ac88097ce9ed6b76bc36a1b377a5e81effdf9b
Trent Billington <pudgepacket@gmail.com>
2017-05-15 15:06:08 +1000

    docs: add sami config

---
+++ sami.cfg.php
@@ -0,0 +1,14 @@
+<?php
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('proto')
+    ->exclude('Internal')
+    ->in('src/Sajari')
+;
+
+return new Sami($iterator);
