bfc028bb49b92f3956ad39ca8e70f21eb098f4ac
David Howden <dhowden@gmail.com>
2017-05-15 22:00:37 +1000

    doc: more documentation for {Add,Get}Response

--- src/Sajari/AddResponse.php
+++ src/Sajari/AddResponse.php
@@ -2,24 +2,54 @@
 
 namespace Sajari;
 
+/**
+ * Class AddResponse
+ *
+ * @package Sajari
+ */
 class AddResponse
 {
+    /** @var Key $key */
     private $key;
+    /** @var Status $status */
     private $status;
 
+    /**
+     * AddResponse constructor.
+     *
+     * @param Key $key
+     * @param Status $status
+     */
     public function __construct(Key $key, Status $status) {
         $this->key = $key;
         $this->status = $status;
     }
 
+    /**
+     * Returns the status for this response.
+     *
+     * @return Status
+     */
     public function getStatus() {
         return $this->status;
     }
 
+    /**
+     * Returns true iff the status for this response
+     * indicates that an error occurred.
+     *
+     * @return bool
+     */
     public function isError() {
         return $this->getStatus()->isError();
     }
-    
+
+    /**
+     * Returns the Key for this response, only valid
+     * if isError() returns false.
+     *
+     * @return Key
+     */
     public function getKey() {
         return $this->key;
     }
--- src/Sajari/GetResponse.php
+++ src/Sajari/GetResponse.php
@@ -2,24 +2,54 @@
 
 namespace Sajari;
 
+/**
+ * Class GetResponse
+ *
+ * @package Sajari
+ */
 class GetResponse
 {
+    /** @var array $record */
     private $record;
+    /** @var Status $status */
     private $status;
 
+    /**
+     * GetResponse constructor.
+     *
+     * @param array $record
+     * @param Status $status
+     */
     public function __construct(array $record, Status $status) {
         $this->record = $record;
         $this->status = $status;
     }
 
+    /**
+     * Returns the status for this response.
+     *
+     * @return Status
+     */
     public function getStatus() {
         return $this->status;
     }
 
+    /**
+     * Returns true iff the status for this response
+     * indicates that an error occurred.
+     *
+     * @return bool
+     */
     public function isError() {
         return $this->getStatus()->isError();
     }
-    
+
+    /**
+     * Returns the record for this response, only valid
+     * if isError() returns false.
+     *
+     * @return Key
+     */
     public function getRecord() {
         return $this->record;
     }
